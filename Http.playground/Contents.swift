9a129dea8cf708480b0a1b007177e488fbc5f4ee
kanyinda <kanyinda@uqtr.ca>
2022-04-28 18:24:36 -0400

    fin HTTP recuperan le donner et l'adapdatant a L,objet poste creerr

--- Http.playground/Contents.swift
+++ Http.playground/Contents.swift
@@ -2,20 +2,36 @@ import UIKit
 
 
 struct Post:Codable{
-    let userdId:Int
+    let userdId:Int?
     let id :Int
     let title:String
     let  body :String
+// personalisation object json correspondant
+//
+//    enum CodingKeys: String, CodingKey {
+//           case userdId = "userdId"
+//           case id = "id"
+//           case title = "title"
+//          case body = "body"
+//
+//    }
     
 }
-let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!
+//let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!//just 1[Post]
+let url = URL(string: "https://jsonplaceholder.typicode.com/posts/")!//array[Post]
 let task = URLSession.shared.dataTask(with: url){data ,response, error in
-    print("xxxxx")
+ 
         //second way get like object poost via decoders
-    let decoder = JSONDecoder ()
-    if let data = data, let post = try? decoder.decode(Post.self, from: data ){
-        print("ffff"+post.title)
-       }
-}
+    let decoder = JSONDecoder()//create decoder
+    if let data = data{
+        do{
+            let post = try decoder.decode([Post].self, from: data )
+            
+            print(post)
+            print("\(post.count) je suis la voi de l raison ")
+        }catch{
+            print(error)
+        }
+       
+    }}
 task.resume()
-print("xxxxx")
