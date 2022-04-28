5427a992388b197fb7c65e201809bd006cbaa7e2
kanyinda <kanyinda@uqtr.ca>
2022-04-28 16:04:07 -0400

    FIN 1er partie récupérer sous format string le data  de la requêtes

--- Http.playground/Contents.swift
+++ Http.playground/Contents.swift
@@ -1,23 +1,11 @@
 import UIKit
 
 
-struct Post:Codable{
-    let userdId:Int
-    let id :Int
-    let title:String
-    let  body :String
-    
-}
-let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!
+let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!//optionnal Object
 let task = URLSession.shared.dataTask(with: url){data ,response, error in
-    //let decoder = JSONDecoder()
-// FISt way get optional in string
+// First way get optional in string
     if let data = data, let string = String( data:data,encoding: .utf8){
         print(string)
     }
-        //second way get like object poost via decoder
-   //  if let data = data, let string = String( data:data,encoding: .utf8){
-      //     print(string)
-     //  }
 }
 task.resume()
